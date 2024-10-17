5792058a558111b69a83592178b17e2b76af588b
ARSHamirali <arshamirali604@gmail.com>
2024-10-17 16:39:16 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,52 @@
+
+
+// Q1
+// function sum(){
+// var a =10
+// var b =20
+// var c=a+b
+// console.log(c)
+// }
+// sum()
+
+// Q2
+// function leapyear(){
+// if(){
+
+// }
+// }
+
+// leapyear(+prompt("Enter any year"))
+
+
+
+// Q3
+// function triangle(a,b,c){
+// var s = (a+b+c)/2
+// var area = s*(s-a)*(s-b)*(s-c)
+// console.log(area);
+
+// }
+// triangle(+prompt("enter first side of triangle"),+prompt("enter second side of triangle"),+prompt("enter third side of triangle"))
+
+//  Q4
+// function Result(a,b,c){
+// var totalMarks=50
+//     var Astudent=(a/totalMarks)*100
+//  var Bstudent=(b/totalMarks)*100 
+//  var Cstudent=(c/totalMarks)*100   
+// console.log(`Student A ${Astudent} <br> Student B ${Bstudent} <br> Student C ${Cstudent}`);
+
+// }
+
+// Result(+prompt("Enter 1st student number"),+prompt("Enter 2nd student number"),+prompt("Enter 3rd student number"))
+
+
+// function mobile(mobileimage){
+// alert("hello")
+
+// }
+
+// function addimage(addimage1){
+// addimage1.
+// }
